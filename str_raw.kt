6b74c56f4c91af773354c4dbd1a34aee9c2af39f
Konsei0516 <masaya19960516@gmail.com>
2020-10-10 00:03:42 +0900

    修正

--- str_raw.kt
+++ str_raw.kt
@@ -1,4 +1,2 @@
-fun main(args:Array<String>){
-  var msg="""「Kotlin」は、「コトリン」と読みます。AndroidStudio3でサポートされています。"""
-  println(msg)
-}
+var msg="""「Kotlin」は、「コトリン」と読みます。AndroidStudio3でサポートされています。"""
+println(msg)
--- str_template.kt
+++ str_template.kt
@@ -1,5 +1,4 @@
-fun main(args:Array<String>){
-  var value= 10
-  var data =arrayOf(100,200,300)
-  println("変数は${value}")
-}
+var value = 10
+var data = arrayOf(100,200,300)
+println("変数は${value}")
+println("配列の末尾の値は${data[2]}")
